b298071d8c01ad0434bd34852a95bfabfbbdaffb
mihaiwebdev <1mihaisava1@gmail.com>
2023-01-15 22:17:51 +0200

    allowed hosts update

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('PRODUCTION_SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost',
-                 'web-production-83f0.up.railway.app']
+                 'pro-shop.up.railway.app']
 
 
 # Application definition
